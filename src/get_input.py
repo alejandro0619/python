1c9b80ebbb18a6dc2fa62a4033392e3b29b31977
alejandro Lopez <alejandrolpz0619@gmail.com>
2021-08-16 14:14:59 -0400

    🐍Changes: get_input, numbers

---
+++ src/get_input.py
@@ -0,0 +1,3 @@
+# get input from the user and stores it.
+something = input("Type something")
+print(something)
---
+++ src/numbers.py
@@ -0,0 +1,25 @@
+# importing:
+from math import *
+
+int_variables = 5
+float_variables = 4.5
+
+# we can add, subs, multiply and divide as below:
+int_variables + float_variables
+int_variables - float_variables
+int_variables * float_variables
+int_variables / float_variables
+int_variables % float_variables
+
+# parse a number to a string
+str(int_variables)
+
+# built-in math methods:
+abs(-4) # absolute number
+pow(4,2) # power
+max(4, 9) # return the biggest
+min(4, 2) # returns the smallest
+round(4.5) # returns the number rounded.
+floor(3.8) # round the number down
+ceil(2.8) # round the numbe up
+sqrt(36) # returns the square root of a given number
