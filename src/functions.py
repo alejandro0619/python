50d4219d9266b449fd45311904aed3a31e2c1810
alejandro Lopez <alejandrolpz0619@gmail.com>
2021-08-18 11:32:29 -0400

    🐍Changes: Functions added.

---
+++ src/functions.py
@@ -0,0 +1,10 @@
+# we use def as a keyword to declare a function
+
+def say_hello(name):
+  print("hello world", name)
+def say_bye(name): 
+  print("Bye", name)
+
+name = "Alejandro"
+say_hello(name)
+say_bye(name)
---
+++ src/tuples.py
@@ -0,0 +1,3 @@
+coordinates = (4, 5) #It's inmutable.
+# coordinates[0] = 7  err!
+print(coordinates[0], coordinates[1]) # I can easily access to it.0
