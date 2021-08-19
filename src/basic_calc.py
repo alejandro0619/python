a8c270b51fa2b28291f4af1f8b398642e75afb9a
alejandro Lopez <alejandrolpz0619@gmail.com>
2021-08-18 20:45:57 -0400

    🐍Change: Basic calculator

---
+++ src/basic_calc.py
@@ -0,0 +1,16 @@
+def calculator():
+  num1 = float(input("Enter a number:"))
+  op = input("Enter a operator:")
+  num2 = float(input("Enter a number:"))
+  if op == '+':
+    return round(num1 + num2)
+  elif op == '-':
+    return round(num1 - num2)
+  elif op == '*':
+    return round(num1 + num2)
+  elif op == '/':
+    return round(num1 + num2)
+  else:
+    return 'Idk'
+
+print(calculator()) 
