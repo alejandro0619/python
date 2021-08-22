a3401b1907c6c412253278701d4f7f727c49c869
alejandro Lopez <alejandrolpz0619@gmail.com>
2021-08-22 13:10:47 -0400

    🐍changes: classes.

---
+++ src/classes.py
@@ -0,0 +1,5 @@
+from student_class import Student
+
+student1 = Student("Alejandro", "Lopez", "Business", 7.6, False)
+
+print(student1.first_name)
---
+++ src/student_class.py
@@ -0,0 +1,8 @@
+class Student:
+  def __init__(self, first_name, last_name, major, gpa, is_on_probation):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.major = major
+    self.gpa = gpa
+    self.is_on_probation = is_on_probation
+
